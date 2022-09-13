12d9a13e76e009b266e263b629c8ff7507e0aae1
xTaraz Twilight Forever <xtaraz@studio.why>
2022-09-13 23:57:18 +0400

    Add 2 tasks

--- Programming Practice 1/Programming Practice 1.cpp
+++ Programming Practice 1/Programming Practice 1.cpp
@@ -1,8 +1,36 @@
 ﻿#include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
 
-    std::cout << "Hello World!\n";
+    float x, y, q, w, Result;
+
+    cout << "Задание 1.2:" << endl;
+    
+    cout << "Введите x:" << endl;
+    cin >> x;
+
+    Result = sin(5 * x) + 2 * tan(pow(x,2));    
+    
+    cout << "Результат: " << Result << endl;
+    cout << endl;
+
+        cout << "Задание 1.2:" << endl;
+    
+        cout << "Введите x, а затем y" << endl;
+        cin >> q >> y;
+
+        Result = 2 * x + 3 * pow(y,2) - sin(3 * x);
+
+        cout << "Результат: " << Result << endl;
+        cout << endl;
+
+
+
+
+    return 0;
 }
